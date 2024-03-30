d2f37c048980ad4657383fdd7c4947f409ee63d0
unknown <rbharathkumar12@gmail.com>
2024-03-30 14:35:40 +0530

    import export 5 patterns - pattern one

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const superhero = require("./superhero");
-const batman = new superhero("batman");
-console.log(batman.getName());
-batman.setName("newbatman");
-console.timeLog(batman.getName());
-
-const superman = new superhero("rab");
-console.log(superman.getName());
-superman.setName("newsuperman");
-console.timeLog(superman.getName());
+const add = require("./math");
+console.log(add(1, 2));
