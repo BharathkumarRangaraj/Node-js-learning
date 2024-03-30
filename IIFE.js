f226618794891e1c70b1e14b6d892113c9394144
unknown <rbharathkumar12@gmail.com>
2024-03-30 13:42:26 +0530

    module catchng

--- IIFE.js
+++ IIFE.js
@@ -1,4 +1,16 @@
 (function (message) {
-  const name = "superhero";
+  const name = "hey all";
   console.log(message, name);
+})("hey");
+(function (message) {
+  const name = "hey all";
+  console.log(message, name);
+})("hey");
+(function (message) {
+  const name = "hey all";
+  console.log(message, name);
+})("hey");
+(function (message) {
+  console.log(message, name);
+  const name = "hey all";
 })("hey");
--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-require("./superman");
-require("./batman");
-console.log("hey");
+const superhero = require("./superhero");
+// const batman = new superhero("batman");
+console.log(superhero.getName());
+superhero.setName("nagul");
+console.log(superhero.getName());
+
+const newsuperhero = require("./superhero");
+console.log(newsuperhero.getName());
