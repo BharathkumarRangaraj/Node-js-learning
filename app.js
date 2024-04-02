0f33c6e76e60eb9bf098b25f4103139e63d7e4dd
unknown <rbharathkumar12@gmail.com>
2024-04-02 15:38:13 +0530

    fs pipe ,zlib,gz

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const fs = require("node:fs");
+const zlib = require("node:zlib");
 
+const gzib = zlib.createGzip();
 const readablestram = fs.createReadStream("text.txt", {
   encoding: "utf-8",
   highWaterMark: 2,
 });
 
-const writablestream = fs.createWriteStream("text2.txt");
+const writablestream = fs.createWriteStream("text2.txt.gz");
 
-readablestram.on("data", (chunk) => {
-  console.log(chunk);
-  writablestream.write(chunk);
-});
+// readablestram.on("data", (chunk) => {
+//   console.log(chunk);
+//   writablestream.write(chunk);
+// });
+readablestram.pipe(writablestream);
+readablestram.pipe(gzib).pipe(fs.WriteStream("text2.txt"));
