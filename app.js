c24290cee648bbc4e51b93ab4c7b7f7d233a3253
unknown <rbharathkumar12@gmail.com>
2024-04-02 17:29:37 +0530

    Http Routing

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const name = "bharath";
-  res.writeHead(200, { "contetx-Type": "text/html" });
+  if (req.url === "/") {
+    res.writeHead(200, { "content-Type": "text/plain" });
+    res.end("home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-Type": "text/plain" });
+    res.end("about page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        name: "bharath",
+        age: "24",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("page not found");
+  }
+  // res.end(req.url);
+  // res.writeHead(200, { "contetx-Type": "text/html" });
 
   // fs.createReadStream("index.html").pipe(res);
 
-  let html = fs.readFileSync("index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
+  //for dynamic name change in html file
+  // let html = fs.readFileSync("index.html", "utf-8");
+  // html = html.replace("{{name}}", name);
+  // res.end(html);
 });
 
 server.listen(3000, () => {
