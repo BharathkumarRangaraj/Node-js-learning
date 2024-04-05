ddd00bb8b5b75dbc8ce2c3319e522a2f3da4d28e
unknown <rbharathkumar12@gmail.com>
2024-04-05 11:29:18 +0530

    cli option

--- mycustomcl/indexx.js
+++ mycustomcl/indexx.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
-
-console.log("hey code index");
+console.log(process.argv);
+const pokemandata = async (pokenmanname) => {
+  const names = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokenmanname}`
+  );
+  const json = await names.json();
+  const moves = json.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+pokemandata("charmander");
