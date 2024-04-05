593a9f68b9ca85b089a4750dffb3f56b578040f4
unknown <rbharathkumar12@gmail.com>
2024-04-05 11:44:36 +0530

    cli interactive option

--- mycustomcl/indexx.js
+++ mycustomcl/indexx.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-console.log(process.argv);
+
+// console.log(process.argv);
+const inquirer = require("inquirer");
+const { argv } = require("yargs");
 const pokemandata = async (pokenmanname) => {
   const names = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokenmanname}`
@@ -8,4 +11,15 @@ const pokemandata = async (pokenmanname) => {
   const moves = json.moves.map(({ move }) => move.name);
   console.log(moves.slice(0, 5));
 };
-pokemandata("charmander");
+//create prompt module
+const prompt = inquirer.createPromptModule();
+prompt([
+  {
+    type: "input",
+    name: "pokeman",
+    message: "enter pokeman name to vew",
+  },
+]).then((answer) => {
+  const pokeman = answer.pokeman;
+  pokemandata(pokeman);
+});
